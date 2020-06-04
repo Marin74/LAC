91f23d87acd3671288f599fc061114cab4f418fb
Marin Dauriat <4449227+Marin74@users.noreply.github.com>
2020-06-04 22:28:59 +0200

    Remove deprecations

--- web/app.php
+++ web/app.php
@@ -9,7 +9,8 @@ $loader = require __DIR__.'/../app/autoload.php';
 include_once __DIR__.'/../var/bootstrap.php.cache';
 
 $kernel = new AppKernel('prod', false);
-$kernel->loadClassCache();
+Request::setTrustedHeaderName(Request::HEADER_FORWARDED, null);
+Request::setTrustedProxies(['192.0.0.1', '10.0.0.0/8']);
 //$kernel = new AppCache($kernel);
 
 // When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
